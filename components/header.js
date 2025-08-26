d3526b422b356a6faec59755855c943bc329e356
preciousashama <preciousashh97@gmail.com>
2025-08-26 13:13:23 +0100

    workiing on header design almost done

--- components/header.js
+++ components/header.js
@@ -13,36 +13,78 @@ export default function Header() {
       <div className="header-main">
         <div className="header-left logo">
           <Link href="/">
-            <img src="/assets/logo.png" alt="Logo" />
+            <img src="assets/icon.png" alt="Logo" />
           </Link>
         </div>
-
-        <div className="search-container">
-          <form id="search-form">
-            <input type="search" id="search-input" placeholder="Search products..." />
-            <button id="search-button" type="submit">🔍</button>
-          </form>
-        </div>
-
+<div className="search-container">
+  <form id="search-form">
+    <input
+      type="search"
+      id="search-input"
+      placeholder="Search products..."
+    />
+    <button id="search-button" type="submit">
+      <img
+        src="/assets/search.png"
+        alt="Search Icon"
+        className="icon-search"
+      />
+    </button>
+  </form>
+</div>
+    
         <div className="header-right header-icons">
+            <Link href="/currency">
+            <img src="/assets/globe.png" alt="currency" className="icon-currency" />
+          </Link>
+          <Link href="/wishlist">
+            <img src="/assets/heart.png" alt="wishlist" className="icon-wishlist" />
+          </Link>
           <Link href="/cart">
-            <img src="/assets/icons/cart.svg" alt="Cart" className="icon-basket" />
+            <img src="/assets/Basketicon.png" alt="Cart" className="icon-basket" />
           </Link>
           <Link href="/account">
-            <span>Account</span>
+            <span>Sign In / Sign Up</span>
           </Link>
         </div>
       </div>
 
-      <nav className="navbar">
-        <ul className="nav-menu">
-          <li className="nav-item"><Link href="/products">Products</Link></li>
-          <li className="nav-item"><Link href="/stickers">Stickers</Link></li>
-          <li className="nav-item"><Link href="/apparel">Apparel</Link></li>
-          <li className="nav-item"><Link href="/party-occasions">Party</Link></li>
-          <li className="nav-item"><Link href="/subscriptions">Subscriptions</Link></li>
-        </ul>
-      </nav>
+<nav className="navbar">
+  <ul className="nav-menu">
+    <li className="nav-item has-dropdown">
+      <Link href="/Shop">Shop</Link>
+
+      <div className="dropdown-menu">
+        {/* Apparel Section */}
+        <div className="dropdown-section">
+          <span className="dropdown-heading">Apparel</span>
+          <ul>
+            <li><Link href="/Shop/T-shirts">T-shirts</Link></li>
+            <li><Link href="/Shop/Tops">Tops</Link></li>
+            <li><Link href="/Shop/Jeans">Jeans</Link></li>
+            <li><Link href="/Shop/Hoodies">Hoodies</Link></li>
+          </ul>
+        </div>
+
+        {/* Accessories Section */}
+        <div className="dropdown-section">
+          <span className="dropdown-heading">Accessories</span>
+          <ul>
+            <li><Link href="/Shop/Hats">Hats</Link></li>
+            <li><Link href="/Shop/Bags">Bags</Link></li>
+          </ul>
+        </div>
+      </div>
+    </li>
+
+    {/* Other nav items */}
+    <li className="nav-item"><Link href="/Design Artwork">Design Artwork</Link></li>
+    <li className="nav-item"><Link href="/Send Items In">Send Items In</Link></li>
+    <li className="nav-item"><Link href="/Installation">Installation</Link></li>
+    <li className="nav-item"><Link href="/Subscriptions">Subscriptions</Link></li>
+  </ul>
+</nav>
+
     </header>
   );
 }
