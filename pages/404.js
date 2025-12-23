ffcfeeb2791d0dfed6c523df763526bfab8c33d0
Olabode Olusegun <57506509+thewebguyy@users.noreply.github.com>
2025-12-23 13:09:21 +0100

    fixing errors

--- pages/404.js
+++
@@ -1,8 +0,0 @@
-export default function Custom404() {
-  return (
-    <div style={{ padding: "2rem", textAlign: "center" }}>
-      <h1>404 - Page Not Found</h1>
-      <p>Sorry, the page you’re looking for doesn’t exist.</p>
-    </div>
-  );
-}
